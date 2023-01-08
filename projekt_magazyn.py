e98d82c8293078e79d6d5c49c034cdb4f3e61f40
Bellarco92 <joanna.ziemba1@gmail.com>
2023-01-08 19:19:32 +0100

    add methods

--- projekt_magazyn.py
+++ projekt_magazyn.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+import sys
 class Magazyn:
 
     def __init__(self, lista_produktow):
@@ -10,17 +12,32 @@ class Magazyn:
             print(produkt)
 
     def dodaj_produkt(self):
-        self.nazwa_produktu = input('Podaj nazwę produktu: >>>')
+        self.nazwa_produktu = input('Podaj nazwę produktu: >>> ')
         if self.nazwa_produktu not in self.lista_produktow:
             self.lista_produktow.append(self.nazwa_produktu)
         print(f'Produkt {self.nazwa_produktu} został dodany do magazynu.')
 
     def usun_produkt(self):
-        self.nazwa_produktu = input('Podaj nazwę produktu, który chcesz' 
-                                    'usunąć: >>>')
+        self.nazwa_produktu = input('Podaj nazwę produktu, który chcesz usunąć: >>> ')
         if self.nazwa_produktu in self.lista_produktow:
             self.lista_produktow.remove(self.nazwa_produktu)
             print(f'Usunieto produkt {self.nazwa_produktu} z magazynu.')
         else:
             print('Podanego produktu nie ma na magazynie.')
 
+magazyn = Magazyn(['mleko', 'woda', 'jajka'])
+
+while True:
+    print('Wprowadź 1, aby wyświetlić stan magazynu')
+    print('Wprowadź 2, aby dodać produkt.')
+    print('Wprowadź 3, aby usunąć produkt.')
+    print('Wprowadź 4, aby zakończyć.')
+    wybor_uzytkownika = int(input(''))
+    if wybor_uzytkownika == 1:
+        magazyn.wyswietl_dostepne_produkty()
+    elif wybor_uzytkownika == 2:
+        magazyn.dodaj_produkt()
+    elif wybor_uzytkownika == 3:
+        magazyn.usun_produkt()
+    elif wybor_uzytkownika == 4:
+        sys.exit()
